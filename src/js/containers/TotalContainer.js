ab90714e7876b8cfd1fc7716d933f37955d22568
James Itum <jamesitum@protonmail.ch>
2018-09-16 16:01:21 +0800

    conditional style

--- src/js/containers/TotalContainer.js
+++ src/js/containers/TotalContainer.js
@@ -12,14 +12,23 @@ const totalStyle = {
   maxWidth: 323,
 };
 
+const reducetotalStyle = {
+  color: 'white',
+  margin: 'auto',
+  fontSize: 30,
+  paddingTop: 40,
+  maxWidth: 323,
+};
+
 class TotalContainer extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    console.log(this.props.calculate.result.toString().length);
     return (
-      <div style={totalStyle}>
+      <div style={this.props.calculate.result.toString().length > 9 ? reducetotalStyle : totalStyle}>
         {this.props.calculate.result}
       </div>
     );
